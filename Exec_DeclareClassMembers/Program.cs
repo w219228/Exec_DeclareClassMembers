e0e4535bfea8ec7f6f36533ea2072c574da666c3
w2192 <w2192@LAPTOP-2BO0FNQF>
2022-10-24 19:55:41 +0800

    c

---
+++ Exec_DeclareClassMembers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareClassMembers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+	
+	public class FileManager
+	{
+		static void Save()
+		{
+
+		}
+	}
+}
